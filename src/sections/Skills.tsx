2cc6307d5b1c64586df9b4458f663a953d2b88ec
Zabsonk <kapi098.2001@o2.pl>
2025-07-02 23:29:36 +0200

    Added skils levels animation

--- src/sections/Skills.tsx
+++ src/sections/Skills.tsx
@@ -1,21 +1,35 @@
-import {type ReactElement, useRef} from "react";
+import {type ReactElement, useRef, useState} from "react";
 import {skills} from "../constants";
 
 const Skills = (): ReactElement =>{
     const boxRef = useRef(null);
 
+    const [isHovered, setIsHovered] = useState(false);
 
-    return(
+    return (
         <section id={'skills'} className={'skills'}>
-            {
-                skills.map((s:{text: string, imgPath: string}) => (
-                    <div
-                        ref={boxRef}
-                         className={'skill-frame'}>
-                        <img src={s.imgPath} alt="person"/>
-                        <span>{s.text}</span>
+            {skills.map((s: { text: string; imgPath: string; level: number, color: string }, index: number) => (
+                <div ref={boxRef} className={'skill-frame'} key={index}   onMouseEnter={() => setIsHovered(true)}
+                     onMouseLeave={() => setIsHovered(false)}>
+                    <img src={s.imgPath} alt="person"/>
+                    <span>{s.text}</span>
+                    <div className="dots-container">
+                        {[...Array(5)].map((_, i) => (
+                            <div key={i} className="dot-wrapper">
+                                <div className="dot gray"/>
+                                {i < s.level && (
+                                    <div
+                                        className={`dot colored ${isHovered ? 'animate' : ''}`}
+                                        style={{
+                                            animationDelay: `${i * 0.3}s`,
+                                            backgroundColor: s.color
+                                        }}                                    />
+                                )}
+                            </div>
+                        ))}
                     </div>
-                ))}
+                </div>
+            ))}
         </section>
     )
 }
