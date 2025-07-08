51e3f49f7307f038c243224bff426d0c10feedb0
Zabsonk <kapi098.2001@o2.pl>
2025-07-08 22:52:06 +0200

    #### [UPDATE] 0.3.3 Added Blurs to marque

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
+import gsap from 'gsap';
 import App from './App.tsx';
 import "./styles/index.css";
 import "./styles/navbar.css";
@@ -9,6 +9,8 @@ import "./styles/about.css";
 import "./styles/hero.css";
 import "./styles/particleScene.css";
 import "./styles/contact.css";
+import {ScrollTrigger} from "gsap/ScrollTrigger";
+import {SplitText} from "gsap/SplitText";
 
 
 const rootElement = document.getElementById('root');
@@ -16,6 +18,8 @@ const rootElement = document.getElementById('root');
 if (!rootElement) {
     throw new Error("Nie znaleziono elementu z id 'root'.");
 }
+gsap.registerPlugin(SplitText)
+gsap.registerPlugin(ScrollTrigger);
 
 createRoot(rootElement).render(
     <StrictMode>
